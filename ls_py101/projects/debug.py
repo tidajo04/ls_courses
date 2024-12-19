163462e66d31dd03e95be9927763c560e4905681
Tim Johnson <tidajo04@gmail.com>
2024-12-19 18:09:34 +0000

    adding work up to lesson 2 calculator assignment

---
+++ ls_py101/projects/debug.py
@@ -0,0 +1,12 @@
+# debug.py
+
+import pdb
+
+counter = 1
+
+pdb.set_trace()
+
+while counter <= 5:
+    print(counter)
+    pdb.set_trace()  # Add breakpoint
+    counter += 1
--- test_coding/playground.py
+++
@@ -1,2 +0,0 @@
-list = [1, 2, 3, 4, 5, 'dummy', 'thick']
-print(list.index('dummy'))
